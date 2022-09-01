a8337629ad999f65bb71fd33c161bb6cb5c30980
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-09-01 18:26:29 +0300

    fix

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import FriendListItem from './FriendListItem';
+import s from './FriendList.module.css';
 
 export default function FriendList({ friends }) {
   return (
     <ul>
       {friends.map(friend => (
-        <FriendListItem
-          avatar={friend.avatar}
-          name={friend.name}
-          isOnline={friend.isOnline}
-          key={friend.id}
-        />
+        <li className={s.item} key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
       ))}
     </ul>
   );
